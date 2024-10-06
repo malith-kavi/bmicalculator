ed308ab309df39024bbbd802f15f2bd542ce3f00
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-06 23:13:17 +0530

    Update main_page.dart

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -14,31 +14,55 @@ class _MainPageState extends State<MainPage> {
       body: SafeArea(
         child: Container(
           color: Colors.white,
-          child: Column(
+          child: const Column(
             children: [
               Row(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: EdgeInsets.all(8.0),
                     child: Column(
                       children: [
-                        Text("Male icon"),
+                        Icon(Icons.male, size: 150,),
                         Text("Male"),
                       ],
                     ),
                   ),
                   Spacer(),
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: EdgeInsets.all(8.0),
                     child: Column(
                       children: [
-                        Text("Female icon"),
+                        Icon(Icons.female, size: 150,),
                         Text("Female"),
                       ],
                     ),
                   )
                 ],
               ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text("Height"),
+                        Text("176", style: TextStyle(color: Colors.red, fontSize: 50, fontWeight: FontWeight.bold)),
+                        
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.female, size: 150,),
+                        Text("Female"),
+                      ],
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
