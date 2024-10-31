a58f2526d01348e5aa08629b2d7832bd2a31f914
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-31 23:47:45 +0530

    Update main_page.dart

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -13,6 +13,8 @@ class _MainPageState extends State<MainPage> {
   int height = 150;
   int weight = 70;
 
+  String gender = '';
+
   late double bmi = calculateBMI(height: height, weight: weight);
   
 
@@ -23,126 +25,158 @@ class _MainPageState extends State<MainPage> {
         child: Container(
           color: Colors.white,
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              const Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Icon(Icons.male, size: 150,),
-                        Text("Male"),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Icon(Icons.female, size: 150,),
-                        Text("Female"),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 50,),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        const Text("Height"),
-                        Text("$height",
-                        style: kInputLabelColor
+          child: Padding(
+            padding: const EdgeInsets.only(top: 25),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          gender = 'Male';
+                        });  
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: gender == 'Male'? Colors.orange: Colors.yellow,
+                          borderRadius: BorderRadius.circular(25)
+                        ),    
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Icon(Icons.male, size: 150,),
+                              Text("Male"),
+                            ],
+                          ),
                         ),
-                        Row(
-                          children: [
-                            FloatingActionButton(
-                              onPressed: (){
-                                setState(() {
-                                  if (height>50) {
-                                    height--;
-                                    bmi = calculateBMI(height: height, weight: weight);
-                                  }                                 
-                                }); 
-                              },
-                              shape: const CircleBorder(),
-                              child: const Icon(Icons.remove,size:40,),
-                            ),
-                            const SizedBox(width: 25,),
-                            FloatingActionButton(
-                              onPressed: (){
-                                setState(() {
-                                  if (height<220) {
-                                    height++;
-                                    bmi = calculateBMI(height: height, weight: weight);
-                                  }
-                                });
-                              },
-                              shape: const CircleBorder(),
-                              child: const Icon(Icons.add,size:40,),
-                            ),
-                          ],
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                  const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        const Text("Weight"),
-                        Text("$weight",
-                        style: kInputLabelColor
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          gender = 'Female';
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: gender == 'Female'? Colors.orange: Colors.yellow,
+                          borderRadius: BorderRadius.circular(25)
                         ),
-                        Row(
-                          children: [
-                            FloatingActionButton(
-                              onPressed: (){
-                                setState(() {
-                                  if (weight>25) {
-                                    weight--;
-                                    bmi = calculateBMI(height: height, weight: weight);
-                                  }
-                                });
-                              },
-                              shape: const CircleBorder(),
-                              child: const Icon(Icons.remove,size:40,),
-                            ),
-                            const SizedBox(width: 25,),
-                            FloatingActionButton(
-                              onPressed: (){
-                                setState(() {
-                                  if (weight<300) {
-                                    weight++;
-                                    bmi = calculateBMI(height: height, weight: weight);
-                                  }
-                                });
-                              },
-                              shape: const CircleBorder(),
-                              child: const Icon(Icons.add,size:40,),
-                            ),
-                          ],
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Icon(Icons.female, size: 150,),
+                              Text("Female"),
+                            ],
+                          ),
                         ),
-                      ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 50,),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          const Text("Height"),
+                          Text("$height",
+                          style: kInputLabelColor
+                          ),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                onPressed: (){
+                                  setState(() {
+                                    if (height>50) {
+                                      height--;
+                                      bmi = calculateBMI(height: height, weight: weight);
+                                    }                                 
+                                  }); 
+                                },
+                                shape: const CircleBorder(),
+                                child: const Icon(Icons.remove,size:40,),
+                              ),
+                              const SizedBox(width: 25,),
+                              FloatingActionButton(
+                                onPressed: (){
+                                  setState(() {
+                                    if (height<220) {
+                                      height++;
+                                      bmi = calculateBMI(height: height, weight: weight);
+                                    }
+                                  });
+                                },
+                                shape: const CircleBorder(),
+                                child: const Icon(Icons.add,size:40,),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 50,),
-              Column(
-                children: [
-                  const Text("BMI"),
-                  Text(bmi.toStringAsFixed(2),
-                  style: kInputLabelColor.copyWith(color: kTextColorR, fontSize: 60),),
-                ],
-              )
-            ],
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          const Text("Weight"),
+                          Text("$weight",
+                          style: kInputLabelColor
+                          ),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                onPressed: (){
+                                  setState(() {
+                                    if (weight>25) {
+                                      weight--;
+                                      bmi = calculateBMI(height: height, weight: weight);
+                                    }
+                                  });
+                                },
+                                shape: const CircleBorder(),
+                                child: const Icon(Icons.remove,size:40,),
+                              ),
+                              const SizedBox(width: 25,),
+                              FloatingActionButton(
+                                onPressed: (){
+                                  setState(() {
+                                    if (weight<300) {
+                                      weight++;
+                                      bmi = calculateBMI(height: height, weight: weight);
+                                    }
+                                  });
+                                },
+                                shape: const CircleBorder(),
+                                child: const Icon(Icons.add,size:40,),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50,),
+                Column(
+                  children: [
+                    const Text("BMI"),
+                    Text(bmi.toStringAsFixed(2),
+                    style: kInputLabelColor.copyWith(color: kTextColorR, fontSize: 60),
+                    ),
+                    Text(getResult(bmi))
+            
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -151,5 +185,15 @@ class _MainPageState extends State<MainPage> {
 
   double calculateBMI({required int height, required int weight}){
     return (weight/(height*height))*10000;
+  }
+
+  String getResult(bmiValue) {
+    if (bmiValue >= 25) {
+      return "Overweight";
+    } else if (bmiValue > 18.5) {
+      return "Normal Weight";
+    } else {
+      return "Underweight";
+    }
   }
 }
