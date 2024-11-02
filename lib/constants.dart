dd4e3234905d2ba458f9041e4e86a0a1a4d24e3b
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-02 22:18:37 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kTextColor = Color.fromARGB(255, 31, 53, 250);
+const kTextColor = Color.fromARGB(250, 31, 53, 250);
 const kTextColorR = Color.fromARGB(255, 245, 45, 30);
 
 const kInputLabelColor = TextStyle(
