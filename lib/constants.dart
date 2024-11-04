67163077315b98f5f2e6f8a0a0a7a7ab3e5231a0
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-04 18:07:01 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ const kTextColor = Color.fromARGB(255, 31, 53, 250);
 const kTextColorR = Color.fromARGB(255, 245, 45, 30);
 
 const kInputLabelColor = TextStyle(
-  color: kTextColor, 
+  color: Color.fromARGB(255, 0, 0, 0), 
   fontSize: 50, 
   fontWeight: FontWeight.bold,
   );
